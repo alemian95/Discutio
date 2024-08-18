172728cc206bbed77f3a38aade682996fbd9b666
Alessandro Mian <alessandromian95@gmail.com>
2024-08-18 11:40:35 +0200

    Basic seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +15,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
+        $roles = [
+            'superadmin' => Role::create(['name'=>'superadmin']),
+            'admin' => Role::create(['name'=>'admin']),
+            'moderator' => Role::create(['name'=>'moderator']),
+            'user' => Role::create(['name'=>'user']),
+        ];
+
+        $user = User::factory()->create([
+            'name' => env('SUPERADMIN_DEFAULT_NAME'),
+            'email' => env('SUPERADMIN_DEFAULT_EMAIL'),
+            'password' => Hash::make(env('SUPERADMIN_DEFAULT_PASSWORD'))
+        ]);
+
+        $user->assignRole($roles['superadmin']);
+
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
     }
 }
