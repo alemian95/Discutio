4c3549de8e124adfe7473b76e0d546fe248c4ade
Alessandro Mian <alessandromian95@gmail.com>
2024-08-30 00:00:02 +0200

    Category box rounded

--- resources/js/Pages/Themes/default/Dashboard.tsx
+++ resources/js/Pages/Themes/default/Dashboard.tsx
@@ -57,10 +57,10 @@ export default function Dashboard( { categories, threads, category, breadcrumbs,
                                         return (
                                             <>
                                                 <div className='flex flex-col' key={index}>
-                                                    <div className='bg-indigo-50 font-semibold text-indigo-600 p-2 border-b-2 border-indigo-400'>
+                                                    <div className='bg-indigo-50 font-semibold text-indigo-600 p-2 border-b-2 border-indigo-400 rounded-t-md'>
                                                         <Link href={route('dashboard.category', category.code)}>{category.name}</Link>
                                                     </div>
-                                                    <div className='bg-slate-50 py-2 px-4'>
+                                                    <div className='bg-slate-50 py-2 px-4 rounded-b-md'>
                                                         <p>{category.code}</p>
                                                         <p>Threads: <span>{category.threads_count}</span></p>
                                                         <p>Last Thread: <span>{ category.last_thread ? <Link className='text-indigo-700' href={route('threads.show', category.id)}>{category.last_thread.title}</Link> : "No Threads inside this category"}</span></p>
